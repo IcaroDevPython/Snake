ceb698c3468624b0d69287ab527656fb77c5e570
IcaroDevPython <137522485+IcaroDevPython@users.noreply.github.com>
2023-10-22 12:52:07 -0300

    update server.js

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ sockets.on('connection', (socket) => {
 })
 
 server.listen(3000, () => {
-    console.log(`> Server listening on port: 3000`)
+    console.log(`> Server listening on port: 3000 <`)
 })
